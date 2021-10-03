15e8c9546018b4a09363be2abc89c5e4e325e1c4
gorotakeda <rugbytkg3@gmail.com>
2021-10-04 07:10:29 +0900

    投稿検索機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :search_product, only: [:index, :search, :show]
   def index
     @posts = Post.includes(:user).order('created_at DESC')
+    set_post_column
   end
 
   def new
@@ -21,6 +23,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    set_post_column
   end
 
   def destroy
@@ -44,6 +47,10 @@ class PostsController < ApplicationController
     end
   end
 
+  def search
+    @results = @p.result
+  end
+
   private
 
   def post_params
@@ -52,6 +59,17 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def search_product
+    @p = Post.ransack(params[:q]) 
+  end
+
+  def set_post_column
+    @post_sex = Post.select("sex_id").distinct
+    @post_style = Post.select("style_id").distinct
+    @post_purpose = Post.select("purpose_id").distinct
+    @post_age = Post.select("age_id").distinct
   end
 
 end
